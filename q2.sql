86b6f9522c137c965da0fac4e45caa09239f8625
Dongfang <kmomuphnie@gmail.com>
2019-03-13 21:40:42 -0400

    2-6

--- q2.sql
+++ q2.sql
@@ -2,6 +2,7 @@
 SET SEARCH_PATH to parlgov;
 
 DROP TABLE IF EXISTS q2 CASCADE;
+
 CREATE TABLE q2(
 	countryName VARCHAR(50),
 	partyName VARCHAR(100),
@@ -15,39 +16,39 @@ DROP TABLE IF EXISTS partyInfo CASCADE;
 DROP TABLE IF EXISTS Q2Answer CASCADE;
 
 --find out how many cabinets created in each country for the past 20 years
+
 CREATE VIEW numOfCabinet4EachCountry AS
-SELECT country.id, country.name AS countryName, COUNT(cabinet.id) AS numOfCabinet
-FROM country, cabinet
-WHERE country.id = cabinet.country_id AND
-	  cabinet.start_date >= '1996-01-01' AND
-	  cabinet.start_date < '2017-01-01'
-GROUP BY country.id;
+	SELECT country.id, country.name AS countryName, COUNT(DISTINCT cabinet.id) AS numOfCabinet
+	FROM country JOIN cabinet on country.id = cabinet.country_id
+	WHERE cabinet.start_date >= '1996-01-01' AND
+	      cabinet.start_date < '2017-01-01'
+	GROUP BY country.id;
 
 --find out how many cabinets a party has been joined for the past 20 years
 CREATE VIEW numOfCarbinet4EachParty AS
-SELECT cabinet_party.party_id, COUNT(cabinet.id) AS numOfCabinet
-FROM cabinet_party, cabinet
-WHERE cabinet.start_date >= '1996-01-01' AND
-	  cabinet.start_date < '2017-01-01' AND
-	  cabinet_party.cabinet_id = cabinet.id
-GROUP BY cabinet_party.party_id;
+	SELECT cabinet_party.party_id, COUNT(cabinet.id) AS partyCabinetTimes
+	FROM cabinet_party JOIN cabinet on cabinet_party.cabinet_id = cabinet.id
+	WHERE cabinet.start_date >= '1996-01-01' AND
+	  cabinet.start_date < '2017-01-01' 
+	GROUP BY cabinet_party.party_id, cabinet.country_id;
 
 -- find the wanted infomation of all parties
 CREATE VIEW partyInfo AS
-SELECT party_position.party_id, family AS partyFamily, state_market AS stateMarket
-FROM party_family JOIN party_position on party_family.party_id = party_position.party_id;
+	SELECT party_position.party_id, family AS partyFamily, state_market AS stateMarket
+	FROM party_family NATURAL LEFT JOIN party_position;
 
 
 --Answer
 CREATE VIEW Q2Answer
-SELECT countryName,
+	SELECT countryName,
 	   party.name AS partyName,
 	   partyFamily,
 	   stateMarket
-FROM numOfCabinet4EachCountry, numOfCarbinet4EachParty, partyInfo, party
-WHERE numOfCabinet4EachCountry.numOfCabinet = numOfCarbinet4EachParty.numOfCabinet AND
-	  numOfCarbinet4EachParty.party_id = partyInfo.party_id AND
-	  numOfCarbinet4EachParty.party_id = party.id;
+	FROM numOfCabinet4EachCountry, numOfCarbinet4EachParty, partyInfo, party, country
+	WHERE numOfCarbinet4EachParty.partyCabinetTimes = numOfCabinet4EachCountry.numOfCabinet AND
+	  		numOfCarbinet4EachParty.party_id = partyInfo.party_id AND
+	  		numOfCarbinet4EachParty.party_id = party.id AND
+	  		numOfCarbinet4EachParty.country_id = country.id;
 
 
 insert into q2
