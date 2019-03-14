322c0cad9589b28d04a8c1de97167a2a2be94433
Dongfang <kmomuphnie@gmail.com>
2019-03-14 18:41:27 -0400

    asd

--- Assignment2.java
+++ Assignment2.java
@@ -4,7 +4,7 @@ import java.util.List;
 // If you are looking for Java data structures, these are highly useful.
 // Remember that an important part of your mark is for doing as much in SQL (not Java) as you can.
 // Solutions that use only or mostly Java will not receive a high mark.
-//import java.util.ArrayList;
+import java.util.ArrayList;
 //import java.util.Map;
 //import java.util.HashMap;
 //import java.util.Set;
@@ -19,19 +19,54 @@ public class Assignment2 extends JDBCSubmission {
     @Override
     public boolean connectDB(String url, String username, String password) {
         // Implement this method!
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            return true;
+        } 
+        catch(SQLException e){
+            // System.out.println("Connection Failed");
+            return false;
+        }
         return false;
     }
 
     @Override
     public boolean disconnectDB() {
         // Implement this method!
+        if(connection != null) {
+            try {
+                connection.close();
+                return true;
+            } 
+            catch (SQLException se) {
+                System.err.println("SQL Exception." + "<Message>:" + se.getMessage());
+                return false;
+            }
+        }
         return false;
     }
 
     @Override
     public ElectionCabinetResult electionSequence(String countryName) {
         // Implement this method!
-        return null;
+        ElectionCabinetResult answer = new ElectionCabinetResult(new ArrayList<Integer> (), new ArrayList<Integer> () );
+
+        try{
+
+
+
+
+
+
+
+            return answer;
+        }
+
+        catch(SQLException se){
+            System.err.println("SQL EXCEPTION: <MESSAGE:> " + se.getMessage());
+            return null;
+        }
+        //return null;
     }
 
     @Override
@@ -42,8 +77,35 @@ public class Assignment2 extends JDBCSubmission {
 
     public static void main(String[] args) {
         // You can put testing code in here. It will not affect our autotester.
-        System.out.println("Hello");
+        //System.out.println("Hello");
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
