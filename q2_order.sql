67cb5ebe1cec2190c7dd23be761333de5503f450
Dongfang <kmomuphnie@gmail.com>
2019-03-13 20:45:36 -0400

    q2_order 1st

--- q2_order.sql
+++ q2_order.sql
@@ -1,1 +1,9 @@
+
 -- Schema for storing a subset of the Parliaments and Governments database
+SET SEARCH_PATH to parlgov;
+
+SELECT *
+FROM q2
+ORDER BY countryName,
+		 partyName,
+		 stateMarket DESC;
