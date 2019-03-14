5922a8cdb6cbebe2343bd5c70a2026862061b71c
Spike Cui <kmomuphnie@gmail.com>
2019-03-14 19:26:50 -0400

    Update q4.sql

--- q4.sql
+++ q4.sql
@@ -41,8 +41,9 @@ CREATE VIEW combinedInfo AS
 CREATE VIEW avgInfo AS
 	SELECT	countryName, year, country_id,party_id, COALESCE(AVG(votes_percentage),0) AS avg_percentage, partyName
 	FROM combinedInfo
-	GROUP BY year, countryName, country_id, party_id, partyName;
-
+	GROUP BY year, countryName, country_id, party_id, partyName
+	HAVING AVG(votes_percentage) > 0;
+	
 CREATE VIEW Q4Answer AS
 	SELECT year, countryName, partyName,
 			(CASE
