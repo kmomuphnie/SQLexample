f5f472d8d3d81793e25d6f776b262e7769e22f09
Dongfang <kmomuphnie@gmail.com>
2019-03-14 06:10:14 -0400

    asd

--- q4_order.sql
+++ q4_order.sql
@@ -1,1 +1,6 @@
 -- Schema for storing a subset of the Parliaments and Governments database
+SET SEARCH_PATH TO parlgov; 
+
+SELECT * 
+FROM q4 
+ORDER BY year DESC, countryName DESC, voteRange DESC, partyName DESC;
