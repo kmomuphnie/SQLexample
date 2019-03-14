e9eff3714f1b02cf82a8d30a7a430910fb251688
Dongfang <kmomuphnie@gmail.com>
2019-03-13 20:56:07 -0400

    3

--- q2.sql
+++ q2.sql
@@ -1,12 +1,17 @@
 -- Schema for storing a subset of the Parliaments and Governments database
 SET SEARCH_PATH to parlgov;
 
+DROP TABLE IF EXISTS q2 CASCADE;
 CREATE TABLE q2(
-	countryName VARCHAR(50) NOT NULL,
-	partyName name VARCHAR(100) NOT NULL,
-	partyFamily VARCHAR(50) NOT NULL,
-	stateMarket REAL CHECK(state_market >= 0.0 AND state_market <= 10.0)
+	countryName VARCHAR(50),
+	partyName name VARCHAR(100),
+	partyFamily VARCHAR(50),
+	stateMarket REAL
 );
+
+DROP TABLE IF EXISTS numOfCabinet4EachCountry CASCADE;
+DROP TABLE IF EXISTS numOfCarbinet4EachParty CASCADE;
+DROP TABLE IF EXISTS partyInfo CASCADE;
 
 --find out how many cabinets created in each country for the past 20 years
 CREATE VIEW numOfCabinet4EachCountry AS
@@ -19,17 +24,18 @@ GROUP BY country.id;
 
 --find out how many cabinets a party has been joined for the past 20 years
 CREATE VIEW numOfCarbinet4EachParty AS
-SELECT party_id, COUNT(cabinet.id) AS numOfCabinet
+SELECT cabinet_party.party_id, COUNT(cabinet.id) AS numOfCabinet
 FROM cabinet_party, cabinet
 WHERE cabinet.start_date >= '1996-01-01' AND
 	  cabinet.start_date < '2017-01-01' AND
 	  cabinet_party.cabinet_id = cabinet.id
-GROUP BY party_id;
+GROUP BY cabinet_party.party_id;
 
 -- find the wanted infomation of all parties
 CREATE VIEW partyInfo AS
 SELECT party_id, family AS partyFamily, state_market AS stateMarket
 FROM party_family JOIN party_position on party_family.party_id = party_position.party_id;
+
 
 --Answer
 insert into q2
